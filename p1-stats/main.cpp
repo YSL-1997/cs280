7fe095921c54eb397eaca0e0f542404ccb6bd2bb
yusen <liuys1997@gmail.com>
2020-12-12 03:23:53 -0600

    p1 added

---
+++ p1-stats/main.cpp
@@ -0,0 +1,13 @@
+//main.cpp
+#include "stats.h"
+#include "p1_library.h"
+#include <iostream>
+using namespace std;
+
+void test_sum_small_data_set();
+
+int main() {
+  test_sum_small_data_set();
+
+  return 0;
+}
